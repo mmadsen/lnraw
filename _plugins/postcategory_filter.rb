cad6b038d3798437f9058dce4cb8c54b4bd0fbdc
Mark Madsen <mark@madsenlab.org>
2014-07-09 17:17:50 -0700

    commit of lab notes

--- _plugins/postcategory_filter.rb
+++ _plugins/postcategory_filter.rb
@@ -2,6 +2,18 @@ require 'facets/string/titlecase'
 
 module Jekyll
 	module PostAndCategoryFilter
+
+
+
+		def get_category_url(category)
+			urls = { 'project:coarse grained model' => '/projects/coarsegraining',
+						   'project:structured information' => '/projects/structuredinfo',
+						   'project:niche construction' => '/projects/nicheconstruction'}
+			url = urls[category]
+			url
+		end
+
+
 		def posts_without_essays(posts)
 			filtered = []
 			for post in posts
@@ -23,6 +35,12 @@ module Jekyll
 			categories.each { |x| project = strip_prefix_and_titleize_category(x) if x.include? "project"} 
 			project
 		end	
+
+		def get_raw_project_category(categories)
+			project = ""
+			categories.each { |x| project = x if x.include? "project"}
+			project
+		end
 
 		def strip_prefix_and_titleize_category(fullcategory)
 			prefix, sep, cat_name = fullcategory.rpartition(":")
